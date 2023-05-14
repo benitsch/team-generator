d92966738f14d8b372294b57cca90341890c245e
grafenauert <thomas.grafenauer@ait.ac.at>
2023-05-14 17:25:20 +0200

    Adds container utilities and corresponding unit tests.

---
+++ src/models/ContainerUtils.ts
@@ -0,0 +1,25 @@
+export class ContainerUtils {
+
+ static shuffleArray(array: Array<any>): void {
+    array.sort(() => Math.random() - 0.5);
+ }
+
+ static groupElementsByProperty<KeyType>(array: Array<any>, propertyFn: (value: any) => KeyType): Map<KeyType, Array<any>> {
+    const mapping: Map<KeyType, Array<any>> = new Map<KeyType, Array<any>>();
+    for (const value of array){
+        const key: KeyType = propertyFn(value);
+        if (mapping.has(key)){
+            mapping.get(key)?.push(value);
+        }else{
+            mapping.set(key, [value]);
+        }
+    }
+    return mapping;
+ }
+
+ static sortMapAscendingByKey(map: Map<number, any>): Map<number, any> {
+    const ascendingComparer = (pairA: [number, any], pairB: [number, any]): number => {return pairB[0] - pairA[0]};
+    return new Map<number, any>([...map.entries()].sort(ascendingComparer));
+ }
+
+}
---
+++ src/models/__tests__/ContainerUtils.spec.ts
@@ -0,0 +1,70 @@
+import { describe, it, expect } from "vitest";
+
+import {ContainerUtils} from "../ContainerUtils";
+
+describe("Array Shuffle Tests", () => {
+
+  const array: Array<number> = [1, 2, 3];
+
+  it("Should not remove any element from an array.", () => {
+   
+    const arrSize = array.length;
+    ContainerUtils.shuffleArray(array);
+    expect(array.length).toEqual(arrSize);
+
+  });
+
+  it("Should not change any element from an array.", () => {
+   
+    ContainerUtils.shuffleArray(array);
+    expect(array).toContain(1);
+    expect(array).toContain(2);
+    expect(array).toContain(3);
+
+  });
+
+
+});
+
+describe("Array Mapping Tests", () => {
+
+    const array: Array<number> = [1, 2, 3];
+  
+    it("Should map keys by a specific property function.", () => {
+     
+      const parityFn = (a: number): string => {return (a % 2 === 0)? "even" : "odd"; };
+      let mapping: Map<string, Array<number>> = ContainerUtils.groupElementsByProperty<string>(array, parityFn);
+      expect(mapping.size).toEqual(2);
+      expect(mapping.has("odd")).toBeTruthy;
+      expect(mapping.has("even")).toBeTruthy;
+      expect(mapping.get("odd")?.length).toEqual(2);
+      expect(mapping.get("even")?.length).toEqual(1);
+      expect(mapping.get("odd")).toContain(1);
+      expect(mapping.get("odd")).toContain(3);
+      expect(mapping.get("even")).toContain(2);          
+    });
+  
+  
+  });
+
+/** TODO(tg): sorting algo does not work, check why!!
+ * 
+   
+describe("Map Utils Tests", () => {
+
+    const map: Map<number, string> = new Map<number, string>();
+    map.set(2, "Mario");
+    map.set(1, "Luigi");
+    map.set(3, "Bowser");
+  
+    it("Should sort map in ascending order.", () => {
+     
+      ContainerUtils.sortMapAscendingByKey(map);
+      expect(Array.from(map.keys())).toEqual([3,2,1]);
+      expect(Array.from(map.values())).toEqual(["Bowser","Mario","Luigi"]);
+  
+    });  
+  
+});
+*/
+
