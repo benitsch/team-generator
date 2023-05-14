003aa52f2c47ae172e51987274f04a92d9b3d7c0
grafenauert <thomas.grafenauer@ait.ac.at>
2023-05-14 10:21:56 +0200

    Resolves TODOs and adds functionality for sub players in teams.

--- src/models/Team.ts
+++ src/models/Team.ts
@@ -6,12 +6,17 @@ export default class Team {
     private _name = "";
     private _targetSize = 0;
     private _playerList: Array<Player> = [];
+    private _substitutionPlayerList: Array<Player> = [];
 
     constructor(name: string, targetSize: number, playerList: Array<Player>) {
         this._id = uuidv4();
         this._name = name;
         this._targetSize = targetSize;
         this._playerList = playerList;
+    }
+
+    get id(): string {
+        return this._id;
     }
 
     get name(): string {
@@ -27,11 +32,15 @@ export default class Team {
     }
 
     get currentSize(): number{
-        return this._playerList.length;
+        return this._playerList.length + this._substitutionPlayerList.length;
+    }
+
+    get isFull(): boolean {
+        return this.currentSize == this.targetSize;
     }
 
     addPlayer(player: Player): boolean {
-        if (!this._playerList.includes(player) && this.currentSize < this.targetSize){
+        if (!this.isFull && !this.isPlayerInTeam(player)){
             this._playerList.push(player);
             return true;
         }else {
@@ -39,8 +48,37 @@ export default class Team {
         }
     }
 
+    addSubstitutionPlayer(player: Player): boolean {
+        if (!this.isFull && !this.isPlayerInTeam(player)){
+            this._substitutionPlayerList.push(player);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    removePlayer(player: Player): void {
+        const listIndexPlayer: number = this._playerList.indexOf(player, 0);
+        if (listIndexPlayer > -1){
+            this._playerList.splice(listIndexPlayer, 1);
+        }
+
+        const listIndexSubPlayer: number = this._substitutionPlayerList.indexOf(player, 0);
+        if (listIndexSubPlayer > -1){
+            this._substitutionPlayerList.splice(listIndexSubPlayer, 1);
+        }
+    }
+
+    isPlayerInTeam(player: Player): boolean {
+        return this._playerList.includes(player) || this._substitutionPlayerList.includes(player);
+    }
+
     get player(): ReadonlyArray<Player> {
         return this._playerList;
+    }
+
+    get substitutionPlayer(): ReadonlyArray<Player> {
+        return this._substitutionPlayerList;
     }
 
 }
