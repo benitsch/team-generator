72d4fe306066a2c11991e4bcc6ceccb875c69e1c
grafenauert <thomas.grafenauer@ait.ac.at>
2023-06-05 18:40:28 +0200

    Adds unit test to check if teams are created in expected sizes and amounts.

---
+++ src/models/__tests__/TeamGenerator.spec.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from "vitest";
+
+import Team from "../Team";
+import Player from "../Player";
+import GameSkill from "../GameSkill";
+import Game from "../Game";
+import TeamGenerator from "../TeamGenerator";
+import {GeneratorErrorCode } from "../TeamGenerator";
+
+describe("Generate Team Test", () => {
+
+    const game: Game = new Game("HOTS", "MOBA");
+
+
+    it("Shall generate the expected amount of teams of expected target size.", () => {
+
+        const teamSize: number = 3;
+        const amountOfPlayers = 9;
+    
+        let players: Array<Player> = new Array<Player>();
+    
+        for(let i = 0; i < amountOfPlayers; i++){
+            let player: Player = new Player("Player"+i);
+            player.addGameSkill(new GameSkill(game, i%5 + 1));
+            players.push(player);
+        }
+        const generateResult: Array<Team> | GeneratorErrorCode = TeamGenerator.generate(players, teamSize, game);
+        expect(generateResult).instanceOf(Array<Team>);
+
+        const teams: Array<Team> = generateResult as Array<Team>;
+
+        expect(teams.length).toEqual(3);
+
+        for (let team of teams){
+            expect(team.targetSize === teamSize);
+            expect(team.isFull).toBeTruthy;
+        }
+     
+    });
+   
+ 
+  });
